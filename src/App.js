caf71400af2638f347a58c07096ebe85bb60ca9e
Olugbenga Ajewole <123884021+ajewoleOlugbenga@users.noreply.github.com>
2024-01-19 22:46:10 +0100

    working with index Routes

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
       element: <RootLayout />,
       errorElement: <Error/>,
       children: [
-        { path: "/", element: <Home /> },
+        { index: true, element: <Home /> }, //path: ''
         { path: "/products", element: <Products /> },
         {path: '/products/:productId', element: <ProductDetailsPage/>}
       ],
