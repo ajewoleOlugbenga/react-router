2f5e4e84602b39e4221d8c64dc9a8e1e66777794
Olugbenga Ajewole <123884021+ajewoleOlugbenga@users.noreply.github.com>
2024-01-19 14:58:09 +0100

    importing Link instead of using a tag and using to instead of href props

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div>
-      This is my Home Page.
-    </div>
-  )
-}
+    <>
+      <div>This is my Home Page.</div>
+      <p>
+        Go to the 
+        <Link to="/products"> product page</Link>
+      </p>
+    </>
+  );
+};
 
-export default Home
+export default Home;
