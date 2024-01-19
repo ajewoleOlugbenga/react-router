870dd52962352d79ac097ac9726b26aa4a27f24e
Olugbenga Ajewole <123884021+ajewoleOlugbenga@users.noreply.github.com>
2024-01-19 22:36:06 +0100

    making each product path to lead to this page

---
+++ src/pages/ProductDetailsPage.jsx
@@ -0,0 +1,15 @@
+import { useParams, Link } from "react-router-dom";
+
+const ProductDetailsPage = () => {
+  const params = useParams();
+
+  return (
+    <>
+      <h1>The product details</h1>
+      <p>{params.productId}</p>
+      <p><Link to='..'>Back</Link></p>
+    </>
+  );
+};
+
+export default ProductDetailsPage;
