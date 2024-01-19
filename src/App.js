75a6dba2d61258e77ca1325eed591fe819f52427
Olugbenga Ajewole <123884021+ajewoleOlugbenga@users.noreply.github.com>
2024-01-19 15:31:13 +0100

    including children properties to our createBrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./pages/Home";
 import Products from "./pages/Products";
+import RootLayout from './pages/Root'
 
 function App() {
   const router = createBrowserRouter([
-    { path: "/", element: <Home /> },
-    { path: "/products", element: <Products /> },
+    {
+      path: "/",
+      element: <RootLayout />,
+      children: [
+        { path: "/", element: <Home /> },
+        { path: "/products", element: <Products /> },
+      ],
+    },
   ]);
 
   return <RouterProvider router={router} />;
