748b003aa7a7cdcb61b74d627c6c28e62459e7f8
Olugbenga Ajewole <123884021+ajewoleOlugbenga@users.noreply.github.com>
2024-01-19 16:01:46 +0100

    adding an errorElement properties to the creatBrowserRouter

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./pages/Home";
 import Products from "./pages/Products";
 import RootLayout from './pages/Root'
+import Error from "./pages/Error";
 
 function App() {
   const router = createBrowserRouter([
     {
       path: "/",
       element: <RootLayout />,
+      errorElement: <Error/>,
       children: [
         { path: "/", element: <Home /> },
         { path: "/products", element: <Products /> },
