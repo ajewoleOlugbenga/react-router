b2d6e64776087a50d50e01cb5a6dfec7e5a2f101
Olugbenga Ajewole <123884021+ajewoleOlugbenga@users.noreply.github.com>
2024-01-19 16:00:41 +0100

    returning each function

--- src/components/mainnavigation/MainNavigation.js
+++ src/components/mainnavigation/MainNavigation.js
@@ -2,16 +2,18 @@ import { Link } from "react-router-dom";
 import classes from "./MainNavigation.module.css";
 
 export default function MainNavigation() {
+  return (
     <header className={classes.header}>
-        <nav>
-  <ul className={classes.list}>
-    <li>
-      <Link to="/">Home</Link>
-    </li>
-    <li>
-      <Link to="/products">Products</Link>
-    </li>
-  </ul>
-  </nav>
-  </header>;
+      <nav>
+        <ul className={classes.list}>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/products">Products</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
--- src/pages/Root.js
+++ src/pages/Root.js
@@ -1,14 +1,16 @@
-import { Outlet } from 'react-router-dom'
-import classes from "./Root.module.css"
-import MainNavigation from '../components/mainnavigation/MainNavigation'
+import { Outlet } from "react-router-dom";
+import classes from "./Root.module.css";
+import MainNavigation from "../components/mainnavigation/MainNavigation";
 
-function RootLayout () {
-<>
-<MainNavigation/>
-<main className={classes.content}>
-    <Outlet/>
-</main>
-</>
+function RootLayout() {
+  return (
+    <>
+      <MainNavigation />
+      <main className={classes.content}>
+        <Outlet />
+      </main>
+    </>
+  );
 }
 
 export default RootLayout;
