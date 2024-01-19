9dc76389ffc279b2cbfa61f3fe7d822ae7e818f6
Olugbenga Ajewole <123884021+ajewoleOlugbenga@users.noreply.github.com>
2024-01-19 14:54:37 +0100

    importing RouteProvideer and passing in the props router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+
 function App() {
-  return <div></div>;
+  const router = createBrowserRouter([
+    { path: "/", element: <Home /> },
+    { path: "/products", element: <Products /> },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
